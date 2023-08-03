d88f5101f132611d9621d44434bd5472422c650f
cdsiren <charlie@decent.xyz>
2023-08-03 16:41:22 -0400

    prohibition working with cellular genesis nft

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,42 +1,32 @@
 import { TheBox } from "@decent.xyz/the-box";
 import { ethers } from "ethers";
 import { toast } from "react-toastify";
-import { useState } from "react";
-import NumberTicker from "./NumberTicker";
 import { useAccount, useSigner } from "wagmi";
-import { getContractFee } from "../lib/getContractFee";
-
-{/* IMPORTANT UPDATE: need to make sure the mint params are valid for your NFT.  The information below is works for all Decent NFTs & should serve as a good example of what correct inputs look like.  If you are using a Decent NFT, you do not need to change this!  If you are not, then you do need to update the abi and params -- the rest of the information SHOULD be set in getStaticProps on index.tsx, but be sure to double check. */}
 
 const Box = (props:any):JSX.Element => {
   const { address: account } = useAccount();
   const { data: signer } = useSigner();
-  const [quantity, setQuantity] = useState(1);
 
   return <div>
     <div className='text-xl font-[400] pb-4'>Mint:</div>
-    {/* Can delete maxQuantity if you do not want to limit the number of NFTs a person can mint at once */}
-    <div className="pb-6">
-      <NumberTicker quantity={quantity} setQuantity={setQuantity} maxQuantity={10} />
-    </div>
     {/* ----------------------------------------------------------- */}
     <TheBox
-      className={`${props.className}`}
+      className={``}
       signer={signer || null}
       nftParams={{
-        address: props.constants.address,
-        chainId: props.constants.chainId,
+        address: "0xc14F5c50AE7fE363855EFC8DFa55d2833586c893",
+        chainId: 42161,
         paymentToken: ethers.constants.AddressZero,
         mintParams: {
-          abi: "function mint(address to,uint256 numberOfTokens) payable",
-          params: [account, quantity],  
-          cost: ethers.utils.parseEther(props.constants.mintPrice).add(getContractFee(props.constants.chainId) || 0).mul(quantity),
-          endSupply: {// only need one of the below
-            maxCap: props.constants.maxTokens,
-            // sellOutDate: props.constants.sellOutDate,
+          abi: "function purchaseTo(address _to, uint256 _projectId) payable",
+          // Cellular Genesis project id; was able to find ID by searching the contract name in opensea and clicking on the nft to find 5600
+          params: [account, 56],
+          cost: ethers.utils.parseEther("0.01"),
+          endSupply: {
+            maxCap: 10,
           }
         },
-        displayCost: `${props.constants.mintPrice}${' '}${props.constants.chainId === 137 ? 'MATIC' : "ETH"}`
+        displayCost: `0.01 ETH`
       }}
       options={{
         allowSecondary: true,
