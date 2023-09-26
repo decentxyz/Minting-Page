71c48f856401b8c478f9722609a1bc7ff8c063c7
Charlie Durbin <charlie@Charlies-MacBook-Pro.local>
2023-09-26 17:15:38 -0400

    refactor wallet setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,10 @@ import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
+  getDefaultWallets,
   RainbowKitProvider,
   lightTheme
 } from '@rainbow-me/rainbowkit';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-  injectedWallet,
-  rainbowWallet,
-  metaMaskWallet,
-  coinbaseWallet,
-  phantomWallet,
-  walletConnectWallet,
-} from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
   mainnet,
@@ -31,36 +23,26 @@ import {
 } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
- 
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora, avalanche, fantom, moonbeam ],
+  [mainnet, polygon, optimism, arbitrum, base, zora, avalanche, fantom, moonbeam],
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string }),
     publicProvider()
   ]
 );
 
-const walletConnectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID as string;
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Box Demo',
-    wallets: [
-      injectedWallet({ chains }),
-      phantomWallet({ chains }),
-      rainbowWallet({ projectId: walletConnectId, chains }),
-      metaMaskWallet({ projectId: walletConnectId, chains }),
-      coinbaseWallet({ chains, appName: 'Based NFTs' }),
-      walletConnectWallet({ projectId: walletConnectId, chains }),
-    ],
-  },
-]);
+const { connectors } = getDefaultWallets({
+  appName: 'Box Demo',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_ID as string,
+  chains
+});
 
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
   publicClient
-});
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
