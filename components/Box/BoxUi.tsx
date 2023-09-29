0137da950c15c249ceb0b6de2e9e382b59703d59
Charlie Durbin <charlie@decent.xyz>
2023-09-29 11:58:24 -0400

    demo

--- components/Box/BoxUi.tsx
+++ components/Box/BoxUi.tsx
@@ -1,22 +1,23 @@
 import {
   ChainSelector,
   TokenSelector,
-  TokenInfo
 } from "@decent.xyz/box-ui";
 import { useSourceContext } from "./contexts/SourceContext";
 import { ChainId } from "@decent.xyz/box-ui";
+import { useAccount } from "wagmi";
+
+// NOTE: this section I believe you have your own designs for; however, including here for completeness.
 
 const SourceSelectors = () => {
   const { srcChainId, setSrcChainId, srcToken, setSrcToken } = useSourceContext();
+  const { address: sender } = useAccount();
 
   return (
     <>
       <div className='space-y-2 mb-4 flex items-center gap-8'>
-        {/* Headless network selector so can style accordingly; know this might not factor into Unlock designs */}
         <ChainSelector srcChainId={srcChainId} setSrcChainId={setSrcChainId} chains={[ChainId.ETHEREUM, ChainId.ARBITRUM, ChainId.OPTIMISM, ChainId.POLYGON, ChainId.BASE]} />
         
-        {/* Similarly: headless token selector, returning user's balances on each chain.  Know this might not directly factor into Unlock designs, but you will likely want to use our method of retreiving token balances */}
-        {tokens && <TokenSelector srcToken={srcToken} setSrcToken={setSrcToken} tokens={tokens} />}
+        <TokenSelector selectedToken={srcToken} setSelectedToken={setSrcToken} chainId={srcChainId} address={sender} />
       </div>
     </>
   )
